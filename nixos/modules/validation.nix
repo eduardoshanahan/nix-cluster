f58a555580959338cf63b1cec8b3610580fbbe93
Eduardo Shanahan <eduardo.shanahan@arrowheadmind.com>
2026-03-18 09:06:16 +0000

    Fix worker k3s flag generation

--- nixos/modules/validation.nix
+++ nixos/modules/validation.nix
@@ -6,6 +6,10 @@ let
   flags = if clusterEnabled then config.services.k3s.extraFlags else [ ];
   execStart = if clusterEnabled then config.systemd.services.k3s.serviceConfig.ExecStart else "";
   hasWriteKubeconfigMode = builtins.any (flag: lib.hasInfix "--write-kubeconfig-mode" flag) flags;
+  hasClusterCidr = builtins.any (flag: lib.hasInfix "--cluster-cidr" flag) flags;
+  hasServiceCidr = builtins.any (flag: lib.hasInfix "--service-cidr" flag) flags;
+  hasDisableServiceLb = builtins.any (flag: lib.hasInfix "--disable=servicelb" flag) flags;
+  hasDisableTraefik = builtins.any (flag: lib.hasInfix "--disable=traefik" flag) flags;
 in
 {
   assertions = [
@@ -26,8 +30,40 @@ in
       message = "Control-plane nodes must include --write-kubeconfig-mode in their k3s flags.";
     }
     {
+      assertion = (!(clusterEnabled && isServer)) || hasClusterCidr;
+      message = "Control-plane nodes must include --cluster-cidr in their k3s flags.";
+    }
+    {
+      assertion = (!(clusterEnabled && isServer)) || hasDisableServiceLb;
+      message = "Control-plane nodes must disable servicelb in their k3s flags.";
+    }
+    {
+      assertion = (!(clusterEnabled && isServer)) || hasDisableTraefik;
+      message = "Control-plane nodes must disable traefik in their k3s flags.";
+    }
+    {
+      assertion = (!(clusterEnabled && isServer)) || hasServiceCidr;
+      message = "Control-plane nodes must include --service-cidr in their k3s flags.";
+    }
+    {
       assertion = (!(clusterEnabled && (!isServer))) || (!hasWriteKubeconfigMode);
       message = "Worker nodes must not include --write-kubeconfig-mode in their k3s flags.";
+    }
+    {
+      assertion = (!(clusterEnabled && (!isServer))) || (!hasClusterCidr);
+      message = "Worker nodes must not include --cluster-cidr in their k3s flags.";
+    }
+    {
+      assertion = (!(clusterEnabled && (!isServer))) || (!hasDisableServiceLb);
+      message = "Worker nodes must not include --disable=servicelb in their k3s flags.";
+    }
+    {
+      assertion = (!(clusterEnabled && (!isServer))) || (!hasDisableTraefik);
+      message = "Worker nodes must not include --disable=traefik in their k3s flags.";
+    }
+    {
+      assertion = (!(clusterEnabled && (!isServer))) || (!hasServiceCidr);
+      message = "Worker nodes must not include --service-cidr in their k3s flags.";
     }
     {
       assertion = (!(clusterEnabled && isServer)) || (lib.hasInfix "/bin/k3s server" execStart);
